6cb3b12a8a27fbb9b45ed4ee9f46382bec971516
supernoob-zatuyou <68260494+supernoob-zatuyou@users.noreply.github.com>
2020-07-27 12:51:48 +0900

    Update input1.py

--- 基礎/input1.py
+++ 基礎/input1.py
@@ -1,9 +1,16 @@
+#inputの書き方について
+
 def sample_function(x, y):
     z = x + y
     return z
 
-x = int(input())
-y = int(input())
+x = int(input()) # x = input()だとstr型だと認識される？
+y = int(input()) 
+
+#例えば 
+# x = input()   で1を入力
+# y = input()   で2を入力にしてしまうと、
+# print(z)は"12"となってしまい、単に文字としての"1"と"2"を合わせただけになる。
 
 z = sample_function(x, y)
 print(z)
