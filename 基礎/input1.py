028ddcbd77487f4313d79ecf6430044daa564a18
supernoob-zatuyou <68260494+supernoob-zatuyou@users.noreply.github.com>
2020-07-27 13:02:31 +0900

    master

--- 基礎/input1.py
+++ 基礎/input1.py
@@ -1,16 +1,17 @@
 #inputの書き方について
 
-def sample_function(x, y):
+def sample_function(x, y): #関数の定義
     z = x + y
-    return z
+    return z # zの計算結果を返す。
 
-x = int(input()) # x = input()だとstr型だと認識される？
+x = int(input()) # x = input()だとstr型だと認識される。
 y = int(input()) 
 
-#例えば 
+# 例えば 
 # x = input()   で1を入力
 # y = input()   で2を入力にしてしまうと、
 # print(z)は"12"となってしまい、単に文字としての"1"と"2"を合わせただけになる。
+# int関数を使う事で文字列を整数に変換して取得する。
 
 z = sample_function(x, y)
 print(z)
